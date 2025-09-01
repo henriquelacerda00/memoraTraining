a63468b086d0ca4eb8ff4a407b11ce95f6d7fee0
henrique.pineli <henrique.pineli@memora.com.br>
2025-09-01 09:36:17 -0300

    Implements login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-   
+   {
+    path: 'login',
+    component: LoginComponent
+   }
 ];
