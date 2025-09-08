fef37515a9fa5943d284c8e397b04104e03368be
lucas.cordeiro <lucas.cordeiro@memora.com.br>
2025-09-08 12:55:07 -0300

    change rating to component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,48 +20,4 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements AfterViewInit {
-  constructor(private elementRef: ElementRef) {}
-
-  value = 4.6;
-
-  ngAfterViewInit() {
-    const stars = this.elementRef.nativeElement.querySelectorAll('.p-rating-icon-active');
-
-    if (this.value % 1 !== 0 && typeof document !== 'undefined') {
-      const svgNS = 'http://www.w3.org/2000/svg';
-      const gradient = document.createElementNS(svgNS, 'linearGradient');
-      gradient.setAttribute('id', 'starPartialFill');
-      gradient.setAttribute('x1', '0%');
-      gradient.setAttribute('x2', '0%');
-      gradient.setAttribute('y1', '100%');
-      gradient.setAttribute('y2', '0%');
-
-      // Define os stops do gradiente conforme a porcentagem
-      const percent = this.value % 1; // ex: 0.3 para 3.3
-      const stop1 = document.createElementNS(svgNS, 'stop');
-      stop1.setAttribute('offset', `${percent * 100}%`);
-      stop1.setAttribute('stop-color', 'gold');
-
-      const stop2 = document.createElementNS(svgNS, 'stop');
-      stop2.setAttribute('offset', `${percent * 100}%`);
-      stop2.setAttribute('stop-color', 'white');
-
-      gradient.appendChild(stop1);
-      gradient.appendChild(stop2);
-
-      // Adiciona o gradiente ao SVG
-      const star = stars[stars.length - 1].children[0] as SVGElement;
-      star.ownerSVGElement?.querySelector('defs')?.appendChild(gradient);
-
-      const path = star.children[0] as SVGPathElement;
-
-      path.classList.add('star-partial');
-
-      // Aplica o gradiente como fill
-      setTimeout(() => {
-        path.setAttribute('fill', 'url(#starPartialFill)');
-      }, 0);
-    }
-  }
-}
+export class HomeComponent {}
