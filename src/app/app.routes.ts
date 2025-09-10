f9d87b4808b69f9b62d9afc2b621178c98e944af
leandro.brito <leandro.brito@memora.com.br>
2025-09-10 08:56:05 -0300

    feat: adiciona tela notfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,13 @@ export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
   },
 ];
