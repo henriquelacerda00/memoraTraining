f98e7798f61f68e655bd5b644b490367da525d18
henrique.pineli <henrique.pineli@memora.com.br>
2025-09-15 13:57:27 -0300

    fix error app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
-
+// src/main.ts ou app.config.ts
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { routes } from './app.routes';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-import { provideRouter } from '@angular/router';
-import { withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AuthInterceptor } from './core/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+// Registrar locale brasileiro
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
